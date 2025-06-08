1e49341cd46deba87d9117ab65fa251e44acc3f0
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-06-08 15:46:28 +0530

    Some New Changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
+connectDB();
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => res.send("API is Working"));
+app.get("/", (req, res) => res.send("API is Working fine"));
 
 const PORT = process.env.PORT || 3000;
 
