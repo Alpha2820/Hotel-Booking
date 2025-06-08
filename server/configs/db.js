d97cb3a50e2a3695b5cdf9634023cde9b2bd00d5
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-06-08 15:51:23 +0530

    Some Changes done in db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    mongoose.connection.on("Connected", () =>
+    mongoose.connection.on("connected", () =>
       console.log("Database Connected")
     );
+
     await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
   } catch (error) {
     console.log(error.message);
