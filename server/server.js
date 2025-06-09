58ca1bffcc78c47480d590787c1bbfa16b753a2a
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-06-09 12:26:47 +0530

    Some changes

--- server/server.js
+++ server/server.js
@@ -2,10 +2,20 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 const app = express();
 app.use(cors());
+
+// Middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API for Clerk Webhook
+
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is Working fine"));
 
