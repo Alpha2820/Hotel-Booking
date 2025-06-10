d33f1d4776cfbb2c2e205258471877734d94c37e
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-06-10 14:45:44 +0530

    Some Changes Done

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -3,6 +3,6 @@ import { protect } from "../middleware/authMiddleware.js";
 import { registerHotel } from "../controllers/hotelController.js";
 
 const hotelRouter = express.Router();
-hotelController.post("/", protect, registerHotel);
+hotelRouter.post("/", protect, registerHotel);
 
 export default hotelRouter;
